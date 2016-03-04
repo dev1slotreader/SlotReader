6220e14984afe1a032013dc96d7d70ab61ebb498
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-04 16:52:42 +0200

    Improve onClickListener

--- POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
+++ POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
@@ -3,18 +3,16 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.NumberPicker;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    NumberPicker picker1;
-    NumberPicker picker2;
-    Runnable moveValue1;
-    Runnable moveValue2;
-    Handler handler1;
-    Handler handler2;
     int requiredValue = 0;
+    ArrayList<NumberPicker> pickers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,64 +23,54 @@ public class MainActivity extends AppCompatActivity {
                 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
         };
 
-        picker1 = (NumberPicker)findViewById(R.id.picker1);
-        picker1.setMinValue(0);
-        picker1.setMaxValue(letters.length - 1);
-        picker1.setDisplayedValues(letters);
+        LinearLayout pickersContainer = (LinearLayout)findViewById(R.id.pickersContainer);
+        pickers = new ArrayList<>();
+        for(int i = 0; i <= pickersContainer.getChildCount(); i++) {
+            View v = pickersContainer.getChildAt(i);
+            if (v instanceof NumberPicker)
+                pickers.add((NumberPicker)v);
+        }
+        for(NumberPicker picker : pickers){
+            picker.setMinValue(0);
+            picker.setMaxValue(letters.length - 1);
+            picker.setDisplayedValues(letters);
+        }
 
-        picker2 = (NumberPicker)findViewById(R.id.picker2);
-        picker2.setMinValue(0);
-        picker2.setMaxValue(letters.length - 1);
-        picker2.setDisplayedValues(letters);
-
-        handler1 = new Handler();
-        moveValue1 = new Runnable() {
-            @Override
-            public void run() {
-                Method method;
-                try {
-                    method = picker1.getClass().getDeclaredMethod("changeValueByOne", boolean.class);
-                    method.setAccessible(true);
-                    if (picker1.getValue() != requiredValue)
-                        method.invoke(picker1, true);
-                    if(picker1.getValue() != requiredValue - 1)
-                        handler1.postDelayed(moveValue1, 50);
-                } catch (final NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        handler2 = new Handler();
-        moveValue2 = new Runnable() {
-            @Override
-            public void run() {
-                Method method;
-                try {
-                    method = picker2.getClass().getDeclaredMethod("changeValueByOne", boolean.class);
-                    method.setAccessible(true);
-                    if (picker2.getValue() != requiredValue)
-                        method.invoke(picker2, true);
-                    if (picker2.getValue() != requiredValue - 1)
-                        handler2.postDelayed(moveValue2, 50);
-                } catch (final NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
 
         (findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i = 0; i < letters.length; i++) {
-                    if (letters[i].equals("W")) {
-                        requiredValue = i;
-                        if (picker1.getValue() != requiredValue)
-                            moveValue1.run();
-                        if (picker2.getValue() != requiredValue)
-                            moveValue2.run();
+                int i = 0;
+                while (letters[i].equals("W"))
+                    i++;
+
+                for (int pickerIndex = 0; pickerIndex < letters.length; pickerIndex++) {
+                    requiredValue = pickerIndex;
+                    for (pickerIndex = 0; pickerIndex < pickers.size(); pickerIndex++)
+                        if (pickers.get(pickerIndex).getValue() != requiredValue) {
+                            final int finalI = pickerIndex;
+                            new Runnable() {
+                                Method method;
+                                Handler handler = new Handler();
+
+                                @Override
+                                public void run() {
+                                    try {
+                                        NumberPicker picker = pickers.get(finalI);
+                                        method = picker.getClass().getDeclaredMethod("changeValueByOne", boolean.class);
+                                        method.setAccessible(true);
+                                        if (picker.getValue() != requiredValue)
+                                            method.invoke(picker, true);
+                                        if (picker.getValue() != requiredValue - 1)
+                                            handler.postDelayed(this, 50);
+                                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }.run();
+                        }
                     }
                 }
-            }
         });
     }
 }
