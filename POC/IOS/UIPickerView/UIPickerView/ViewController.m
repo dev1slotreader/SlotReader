d33d8970ee29260dd95214e9f0f74313ff8efd33
Anastasiia Yarovenko <deviosslotreader@gmail.com>
2016-03-07 15:31:23 +0200

    Added circularity and selection

--- POC/IOS/UIPickerView/UIPickerView/ViewController.m
+++ POC/IOS/UIPickerView/UIPickerView/ViewController.m
@@ -38,27 +38,51 @@
 }
 
 - (NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-	return [self.colorNamesArray count];
+	return UINT16_MAX;
 }
 
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-	return [self.colorNamesArray objectAtIndex:row];
+	return [self.colorNamesArray objectAtIndex:row % [self.colorsArray count]];
 }
 
 - (void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
-	self.colorLabel.text = [self.colorNamesArray objectAtIndex:row];
-	self.colorLabel.textColor = [self.colorsArray objectAtIndex:row];
+	self.colorLabel.text = [self.colorNamesArray objectAtIndex:row % [self.colorsArray count]];
+	self.colorLabel.textColor = [self.colorsArray objectAtIndex:row % [self.colorsArray count]];
 }
 
 - (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
 	UILabel *pickerLabel = [UILabel new];
-	NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[self.colorNamesArray objectAtIndex:row]];
+	NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[self.colorNamesArray objectAtIndex:row % [self.colorsArray count]]];
 	NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
 	[paragraphStyle setAlignment:NSTextAlignmentCenter];
 	[text addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, text.length)];
 	pickerLabel.attributedText = text;
-	pickerLabel.backgroundColor = [self.colorsArray objectAtIndex:row];
+	pickerLabel.backgroundColor = [self.colorsArray objectAtIndex:row % [self.colorsArray count]];
 	return pickerLabel;
+}
+
+#pragma mark Displaying the words
+
+- (void) displayWord:(NSString *)word animated:(BOOL)animated {
+	for (NSUInteger i = 0; i < [word length]; i++) {
+		NSString *letter = [NSString stringWithFormat:@"%c", [word characterAtIndex:i]];
+		[self displayLetter:letter
+				atComponent:i
+				   animated:animated];
+	}
+}
+
+- (void) displayLetter:(NSString *)letter atComponent:(NSUInteger)component animated:(BOOL)animated {
+	NSUInteger row;
+	if (animated){
+		NSUInteger selectedRow = [self.picker selectedRowInComponent:component];
+		row = selectedRow + ([self.colorNamesArray count] - selectedRow % [self.colorNamesArray count]) + [self.colorNamesArray indexOfObject:letter];
+	}
+	else
+		row = ((UINT16_MAX) / (2 * [self.colorNamesArray count]) * [self.colorNamesArray count]) + [self.colorNamesArray indexOfObject:letter];
+	[self.picker selectRow:row
+					 inComponent:component
+						animated:animated];
 }
 
 @end
