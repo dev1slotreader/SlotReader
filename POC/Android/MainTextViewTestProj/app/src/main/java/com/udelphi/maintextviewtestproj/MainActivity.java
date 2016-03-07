0f45bd18845499293bc9b6ad69a9bf7c47a0a4c1
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-07 19:05:38 +0200

    Reorganize MainTextViewTestProj values moving
     - also add pickers and buttons for program testing

--- POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
+++ POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
@@ -10,17 +10,39 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     int requiredValue = 0;
     ArrayList<NumberPicker> pickers;
+    Handler handler;
+    Runnable movePikersRunnable;
+    String[] letters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String[] letters = new String[]{
+        letters = new String[]{
             "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
                 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+
+        handler = new Handler();
+        movePikersRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Method method = NumberPicker.class.getDeclaredMethod("changeValueByOne", boolean.class);
+                    method.setAccessible(true);
+                    for(NumberPicker picker : pickers) {
+                        if (picker.getValue() != requiredValue)
+                            method.invoke(picker, true);
+                    }
+                    OnPickersMoved();
+
+                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
         };
 
         LinearLayout pickersContainer = (LinearLayout)findViewById(R.id.pickersContainer);
@@ -37,41 +59,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        (findViewById(R.id.btn)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int i = 0;
-                while (!letters[i].equals("W"))
-                    i++;
+        (findViewById(R.id.a_btn)).setOnClickListener(this);
+        (findViewById(R.id.o_btn)).setOnClickListener(this);
+        (findViewById(R.id.z_btn)).setOnClickListener(this);
+    }
 
-                for (int pickerIndex = 0; pickerIndex < letters.length; pickerIndex++) {
-                    requiredValue = i;
-                    for (pickerIndex = 0; pickerIndex < pickers.size(); pickerIndex++)
-                        if (pickers.get(pickerIndex).getValue() != requiredValue) {
-                            final int pickerIndexFinal = pickerIndex;
-                            new Runnable() {
-                                Method method;
-                                Handler handler = new Handler();
+    private void OnPickersMoved(){
+        if (needMoving()) {
+            handler.postDelayed(movePikersRunnable, 75);
+        }
+        else handler.removeCallbacks(movePikersRunnable);
+    }
 
-                                @Override
-                                public void run() {
-                                    try {
-                                        NumberPicker picker = pickers.get(pickerIndexFinal);
-                                        method = picker.getClass().getDeclaredMethod("changeValueByOne", boolean.class);
-                                        method.setAccessible(true);
-                                        method.invoke(picker, true);
-                                        if (picker.getValue() != requiredValue)
-                                            method.invoke(picker, true);
-                                        if (picker.getValue() != requiredValue - 1)
-                                            handler.postDelayed(this, 50);
-                                    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }.run();
-                        }
-                    }
-                }
-        });
+    private boolean needMoving(){
+        boolean result = false;
+        for(NumberPicker picker : pickers) {
+            if (picker.getValue() != requiredValue) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private void setRequiredValue(String symbol){
+        try {
+            while (!letters[requiredValue].equals(symbol))
+                requiredValue++;
+        }catch (IndexOutOfBoundsException e) {
+            requiredValue = 0;
+            setRequiredValue(symbol);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.a_btn:
+                setRequiredValue("A");
+                if (needMoving())
+                    handler.post(movePikersRunnable);
+                break;
+            case R.id.o_btn:
+                setRequiredValue("O");
+                if (needMoving())
+                    handler.post(movePikersRunnable);
+                break;
+            case R.id.z_btn:
+                setRequiredValue("Z");
+                if (needMoving())
+                    handler.post(movePikersRunnable);
+                break;
+        }
     }
 }
