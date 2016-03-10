53e04df1d6323d7e8f8b6f3b9de5e9a29a1c3b2f
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-10 16:10:43 +0200

    Move pickers to separated control

--- POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
+++ POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/MainActivity.java
@@ -1,144 +1,34 @@
 package com.udelphi.maintextviewtestproj;
-import android.content.Context;
-import android.graphics.Paint;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.TextView;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    int requiredValue = 0;
-    ArrayList<NumberPicker> pickers;
-    Handler handler;
-    Runnable movePikersRunnable;
-    String[] letters;
+    WordPicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        letters = new String[]{
-            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-                "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-        };
 
-        handler = new Handler();
-        movePikersRunnable = new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Method method = NumberPicker.class.getDeclaredMethod("changeValueByOne", boolean.class);
-                    method.setAccessible(true);
-                    for(NumberPicker picker : pickers) {
-                        if (picker.getValue() != requiredValue)
-                            method.invoke(picker, true);
-                    }
-                    OnPickersMoved();
+        picker = (WordPicker)findViewById(R.id.word_picker);
 
-                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        LinearLayout pickersContainer = (LinearLayout)findViewById(R.id.pickersContainer);
-        pickers = new ArrayList<>();
-        for(int i = 0; i <= pickersContainer.getChildCount(); i++) {
-            View v = pickersContainer.getChildAt(i);
-            if (v instanceof NumberPicker)
-                pickers.add((NumberPicker)v);
-        }
-        for(NumberPicker picker : pickers){
-            picker.setMinValue(0);
-            picker.setMaxValue(letters.length - 1);
-            picker.setDisplayedValues(letters);
-            try {
-                //change pickers divider color
-                Field field = NumberPicker.class.getDeclaredField("mSelectionDivider");
-                field.setAccessible(true);
-                field.set(picker, getResources().getDrawable(android.R.drawable.screen_background_light));
-                //change pickers text color
-                for(int i = 0; i < picker.getChildCount(); i++) {
-                    View child = picker.getChildAt(i);
-                    if(child instanceof EditText)
-                        ((EditText) child).setTextColor(getResources().getColor(android.R.color.white));
-                }
-                field = NumberPicker.class.getDeclaredField("mSelectorWheelPaint");
-                field.setAccessible(true);
-                ((Paint)field.get(picker)).setColor(getResources().getColor(android.R.color.white));
-                picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
-                picker.invalidate();
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-
-        (findViewById(R.id.a_btn)).setOnClickListener(this);
+                (findViewById(R.id.a_btn)).setOnClickListener(this);
         (findViewById(R.id.o_btn)).setOnClickListener(this);
         (findViewById(R.id.z_btn)).setOnClickListener(this);
-    }
-
-
-    @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        return super.onCreateView(name, context, attrs);
-    }
-
-    private void OnPickersMoved(){
-        if (needMoving()) {
-            handler.postDelayed(movePikersRunnable, 75);
-        }
-        else handler.removeCallbacks(movePikersRunnable);
-    }
-
-    private boolean needMoving(){
-        boolean result = false;
-        for(NumberPicker picker : pickers) {
-            if (picker.getValue() != requiredValue) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private void setRequiredValue(String symbol){
-        try {
-            while (!letters[requiredValue].equals(symbol))
-                requiredValue++;
-        }catch (IndexOutOfBoundsException e) {
-            requiredValue = 0;
-            setRequiredValue(symbol);
-        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.a_btn:
-                setRequiredValue("A");
-                if (needMoving())
-                    handler.post(movePikersRunnable);
+                picker.moveValue("A");
                 break;
             case R.id.o_btn:
-                setRequiredValue("O");
-                if (needMoving())
-                    handler.post(movePikersRunnable);
+                picker.moveValue("O");
                 break;
             case R.id.z_btn:
-                setRequiredValue("Z");
-                if (needMoving())
-                    handler.post(movePikersRunnable);
+                picker.moveValue("Z");
                 break;
         }
     }
---
+++ POC/Android/MainTextViewTestProj/app/src/main/java/com/udelphi/maintextviewtestproj/WordPicker.java
@@ -0,0 +1,183 @@
+package com.udelphi.maintextviewtestproj;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+public class WordPicker extends LinearLayout {
+    public static enum Style { LIGHT, LIGHT_WITH_DIVIDERS, DARK, DARK_WITH_DIVIDERS, DEFAULT }
+
+    int requiredValue = 0;
+    Runnable movePikersRunnable;
+    ArrayList<NumberPicker> pickers;
+    String[] letters;
+    Handler handler;
+    Style style = Style.DEFAULT;
+
+
+
+    public WordPicker(Context context){
+        super(context);
+//        this.style = style;
+        initView(context);
+    }
+
+    public WordPicker(Context context, AttributeSet attrs){
+        super(context, attrs);
+//        this.style = style;
+        initView(context);
+    }
+
+    public WordPicker(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+//        this.style = style;
+        initView(context);
+    }
+
+
+
+    private void initView(Context context) {
+        letters = new String[]{
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+                "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        };
+        movePikersRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Method method = NumberPicker.class.getDeclaredMethod("changeValueByOne", boolean.class);
+                    method.setAccessible(true);
+                    for (NumberPicker picker : pickers) {
+                        if (picker.getValue() != requiredValue)
+                            method.invoke(picker, true);
+                    }
+                    OnPickersMoved();
+
+                } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler = new Handler();
+
+        View view = inflate(context, R.layout.word_picker_layout, this);
+        LinearLayout pickersContainer = (LinearLayout) view.findViewById(R.id.pickersContainer);
+        pickers = new ArrayList<>();
+        for (int i = 0; i <= pickersContainer.getChildCount(); i++) {
+            View v = pickersContainer.getChildAt(i);
+            if (v instanceof NumberPicker)
+                pickers.add((NumberPicker) v);
+        }
+        for (NumberPicker picker : pickers) {
+            picker.setMinValue(0);
+            picker.setMaxValue(letters.length - 1);
+            picker.setDisplayedValues(letters);
+            style = Style.LIGHT_WITH_DIVIDERS;
+            setDividerStyle(picker);
+        }
+
+    }
+
+    public void moveValue(String value){
+        setRequiredValue(value);
+        if(needMoving())
+            handler.post(movePikersRunnable);
+    }
+
+    public void setStyle(Style style){
+        this.style = style;
+    }
+
+    private void setRequiredValue(String symbol){
+        try {
+            while (!letters[requiredValue].equals(symbol))
+                requiredValue++;
+        }catch (IndexOutOfBoundsException e) {
+            requiredValue = 0;
+            setRequiredValue(symbol);
+        }
+    }
+
+    private void OnPickersMoved(){
+        if (needMoving()) {
+            handler.postDelayed(movePikersRunnable, 75);
+        }
+        else handler.removeCallbacks(movePikersRunnable);
+    }
+
+    private boolean needMoving(){
+        boolean result = false;
+        for(NumberPicker picker : pickers) {
+            if (picker.getValue() != requiredValue) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private void setDividerStyle(NumberPicker picker){
+        try {
+            Field mSelectionDivider = NumberPicker.class.getDeclaredField("mSelectionDivider");
+            mSelectionDivider.setAccessible(true);
+            switch (style){
+                case LIGHT_WITH_DIVIDERS:
+                    mSelectionDivider.set(picker, getResources().getDrawable(android.R.drawable.screen_background_light));
+                    setLightText(picker);
+                    break;
+                case LIGHT:
+                    mSelectionDivider.set(picker, getResources().getDrawable(R.drawable.transparent_divider));
+                    setLightText(picker);
+                    break;
+                case DARK_WITH_DIVIDERS:
+                    mSelectionDivider.set(picker, getResources().getDrawable(android.R.drawable.screen_background_dark));
+                    setDarkText(picker);
+                    break;
+                case DARK:
+                    mSelectionDivider.set(picker, getResources().getDrawable(android.R.drawable.screen_background_dark_transparent));
+                    setDarkText(picker);
+                    break;
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setLightText(NumberPicker picker) throws NoSuchFieldException, IllegalAccessException {
+        Field mSelectorWheelPaint = NumberPicker.class.getDeclaredField("mSelectorWheelPaint");
+        mSelectorWheelPaint.setAccessible(true);
+        for (int i = 0; i < picker.getChildCount(); i++) {
+            View child = picker.getChildAt(i);
+            if (child instanceof EditText)
+                ((EditText) child).setTextColor(getResources().getColor(android.R.color.white));
+            ((Paint) mSelectorWheelPaint.get(picker)).setColor(getResources().getColor(android.R.color.white));
+            picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+            picker.invalidate();
+        }
+    }
+
+    private void setDarkText(NumberPicker picker)throws NoSuchFieldException, IllegalAccessException {
+        Field mSelectorWheelPaint = NumberPicker.class.getDeclaredField("mSelectorWheelPaint");
+        mSelectorWheelPaint.setAccessible(true);
+        for (int i = 0; i < picker.getChildCount(); i++) {
+            View child = picker.getChildAt(i);
+            if (child instanceof EditText)
+                ((EditText) child).setTextColor(getResources().getColor(android.R.color.background_dark));
+            ((Paint) mSelectorWheelPaint.get(picker)).setColor(getResources().getColor(android.R.color.background_dark));
+            picker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+            picker.invalidate();
+        }
+    }
+}
