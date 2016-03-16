af86de4339d6d792a6732160538b4cbec42fe6df
Anastasiia Yarovenko <deviosslotreader@gmail.com>
2016-03-16 17:44:45 +0200

    Adding graphic resources Updating design

---
+++ POC/IOS/SlotReader/SlotReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlotReader
+//
+//  Created by User on 3/11/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
