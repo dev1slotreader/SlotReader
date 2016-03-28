a125fc6f2d6b742b85e6d9927b2c88f58f24814d
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-28 16:29:16 +0300

    no message

--- AndroidClient/SlotReader/app/src/main/java/com/udelphi/slotreader/MainActivity.java
+++ AndroidClient/SlotReader/app/src/main/java/com/udelphi/slotreader/MainActivity.java
@@ -3,11 +3,17 @@ package com.udelphi.slotreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.udelphi.slotreader.Abstractions.BoardView;
+
 public class MainActivity extends AppCompatActivity {
+    private String[] enAlphabet = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}; //Remove after parser import
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BoardView boardView = (BoardView) findViewById(R.id.word_picker);
+        boardView.setValues(enAlphabet);
     }
 }
