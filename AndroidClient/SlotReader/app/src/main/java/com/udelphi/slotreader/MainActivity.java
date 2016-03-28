fc2b0f6ac1b89378042589b46c27893ae4eee39b
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-28 12:19:01 +0300

    Create android client project

---
+++ AndroidClient/SlotReader/app/src/main/java/com/udelphi/slotreader/MainActivity.java
@@ -0,0 +1,13 @@
+package com.udelphi.slotreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
