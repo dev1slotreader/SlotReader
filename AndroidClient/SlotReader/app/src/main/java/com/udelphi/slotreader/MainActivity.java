849ec6413b9bfe1e3c39f1473028bc6c34669458
Igor-Martynyuk <martynyuk-i@bk.ru>
2016-03-30 18:17:15 +0300

    Add menu button

--- AndroidClient/SlotReader/app/src/main/java/com/udelphi/slotreader/MainActivity.java
+++ AndroidClient/SlotReader/app/src/main/java/com/udelphi/slotreader/MainActivity.java
@@ -1,18 +1,18 @@
 package com.udelphi.slotreader;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import com.udelphi.slotreader.Fragments.ReaderFragment;
 
-public class MainActivity extends AppCompatActivity implements ListView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private String[] menuItems;
     private DrawerLayout drawerLayout;
     private ListView drawerList;
@@ -22,33 +22,44 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton button = (ImageButton)findViewById(R.id.menu_btn);
+        button.setOnClickListener(this);
         menuItems = getResources().getStringArray(R.array.menu_items);
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawerList = (ListView)findViewById(R.id.left_drawer);
-
         drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
                 menuItems));
-        drawerList.setOnItemClickListener(this);
-        onItemClick(null, null, 0, 0);
+        ListView.OnItemClickListener menuClickListener = new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment fragment;
+                switch (position){
+                    case 0:
+                        fragment = new ReaderFragment();
+                        break;
+                    default:
+                        fragment = null;
+                }
+
+                if(fragment != null){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content_frame, fragment)
+                            .commit();
+                }
+                drawerList.setItemChecked(position, true);
+                drawerLayout.closeDrawer(drawerList);
+            }
+        };
+        drawerList.setOnItemClickListener(menuClickListener);
+        menuClickListener.onItemClick(null, null, 0, 0);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Fragment fragment;
-        switch (position){
-            case 0:
-                fragment = new ReaderFragment();
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.menu_btn:
+                drawerLayout.openDrawer(drawerList);
                 break;
-            default:
-                fragment = null;
         }
-
-        if(fragment != null){
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.content_frame, fragment)
-                    .commit();
-        }
-        drawerList.setItemChecked(position, true);
-        drawerLayout.closeDrawer(drawerList);
     }
 }
