0d6195a3c3619d35ec6b0370a137fae29f494648
Anastasiya Yarovenko <anastasijajarvenk@gmail.com>
2017-02-03 00:43:42 +0200

    Added text to speech feature

---
+++ POC/IOS/SlotReader/SlotReader/CenterViewController+TextToSpeach.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController+TextToSpeach.h
+//  SlotReader
+//
+//  Created by Anastasiya Yarovenko on 22.01.17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import "CenterViewController.h"
+
+@interface CenterViewController (TextToSpeach)
+
+@end
---
+++ POC/IOS/SlotReader/SlotReader/CenterViewController+TextToSpeach.m
@@ -0,0 +1,13 @@
+//
+//  CenterViewController+TextToSpeach.m
+//  SlotReader
+//
+//  Created by Anastasiya Yarovenko on 22.01.17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+#import "CenterViewController+TextToSpeach.h"
+
+@implementation CenterViewController (TextToSpeach)
+
+@end
